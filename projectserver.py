dc907dc9b7fd3c7834784b0eee9a832cba2cb443
JakeVick <47531752+JakeVick@users.noreply.github.com>
2021-11-10 18:39:18 -0500

    Added some server paths and added some libraries

--- projectserver.py
+++ projectserver.py
@@ -1,12 +1,42 @@
-from flask import Flask
-from pymongo import MongoClient
+from flask import Flask, request, redirect, render_template, abort
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello 312"
+    #return "Hello 312"
+    return redirect("/login")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("<html><body>chat page</body></html>")
+    if request.method == 'POST':
+        user = request.form["user"]
+        passwordhash = request.form["password"]
+        #hash the password and then store it in the database using hashlib
+        return redirect("/mainpage")
+
+@app.route('/createaccount')
+def createAccount():
+    return render_template("<html><body>create account page</body></html>")
+
+@app.route('/mainpage')
+def mainpage():
+    return render_template("<html><body>main page</body></html>")
+
+@app.route('/chatpage', methods=['GET', 'POST'])
+def chat():
+    if request.method == 'GET':
+        return render_template("<html><body>chat page</body></html>")
+    if request.method == 'POST':
+        #if it was a form, you can access it on request.form
+        return "wow you made a post request to me!"
+    else:
+        return abort(403)
+
+
 
 
 if __name__ == '__main__':
