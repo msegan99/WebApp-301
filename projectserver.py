7c504d33175519a05ab083f5f76fa1bf748f1fa7
humzamal <humzamal@buffalo.edu>
2021-11-12 18:14:54 -0500

    added some things on the frontend

--- projectserver.py
+++ projectserver.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    #return "Hello 312"
-    return redirect("/login")
+    # return "Hello 312"
+    return render_template("index.html")
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -15,28 +16,29 @@ def login():
     if request.method == 'POST':
         user = request.form["user"]
         passwordhash = request.form["password"]
-        #hash the password and then store it in the database using hashlib
+        # hash the password and then store it in the database using hashlib
         return redirect("/mainpage")
 
-@app.route('/createaccount')
+
+@app.route('/register')
 def createAccount():
-    return render_template("<html><body>create account page</body></html>")
+    return render_template("register.html")
+
 
 @app.route('/mainpage')
 def mainpage():
     return render_template("<html><body>main page</body></html>")
+
 
 @app.route('/chatpage', methods=['GET', 'POST'])
 def chat():
     if request.method == 'GET':
         return render_template("<html><body>chat page</body></html>")
     if request.method == 'POST':
-        #if it was a form, you can access it on request.form
+        # if it was a form, you can access it on request.form
         return "wow you made a post request to me!"
     else:
         return abort(403)
-
-
 
 
 if __name__ == '__main__':
