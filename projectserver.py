d004fad72e9b12448554c402bf581ca97caee321
msegan99 <msegan@buffalo.edu>
2021-11-12 23:45:54 -0500

    Added password hashing/login password validation

--- projectserver.py
+++ projectserver.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, redirect, render_template, abort
-from werkzeug.exceptions import FailedDependency
 from pymongo import MongoClient
+import hashlib, os
 
 app = Flask(__name__)
 client= MongoClient('localhost', 27017)
 db=client["312ProjectDatabase"]
 accountCollection=db["accountCollection"]
 chatCollection=db["chatCollection"]
+userSalts={}
 
 @app.route('/')
 def hello():
@@ -17,12 +18,27 @@ def hello():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template("<html><body>chat page</body></html>")
+        return render_template("login.html")
     if request.method == 'POST':
-        user = request.form["user"]
-        passwordhash = request.form["password"]
-        # hash the password and then store it in the database using hashlib
-        return redirect("/mainpage")
+        username = request.form["username"]
+        password = request.form["password"]
+        if(accountCollection.count_documents({"username": username})==1): #if this username exist
+            accountDocument=accountCollection.find_one({"username": username})
+            hashedPassword=accountDocument["password"]
+            unhashedPasswordKey= hashedPassword[32:] #randomSalt was 32 bytes. key is the rest
+            userSalt=userSalts[username]
+            keyToCheck=hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), userSalt, 100000)
+
+            if keyToCheck==unhashedPasswordKey: #SUCCESSFUL login. password keys match
+                print("Successful login!")
+                return redirect('/mainpage')
+                
+            else: #username exist, but wrong password given
+                return redirect("/login")
+            
+        else: #No such username exists
+            return redirect('/login')
+        
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -36,9 +52,14 @@ def createAccount():
         #***registering users validation should be here***
         if accountCollection.count(query)==1:  #if this username already exists just send back to register page for now
             return redirect('/register') 
-        else:
-            #add username and password to accountCollection
-            document={ "username": username, "password": password}
+
+        else: #successful register
+            #Hash password and then add username and password to accountCollection
+            randomSalt= os.urandom(32)
+            userSalts[username]=randomSalt #store salt so we can verify login attempts
+            saltkey = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), randomSalt, 100000)
+            hashedPassword = randomSalt + saltkey 
+            document={ "username": username, "password": hashedPassword}
             accountCollection.insert(document)
             return redirect('/mainpage')
 
